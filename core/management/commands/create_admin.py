7153d981a63b50f66b02b49b0a9ca1f7d6b965a0
rohanphule <phulerohan0@gmail.com>
2026-04-23 10:51:05 +0530

    Fix create_admin command - force recreate superuser

--- core/management/commands/create_admin.py
+++ core/management/commands/create_admin.py
@@ -1,17 +1,33 @@
 from django.core.management.base import BaseCommand
 from core.models import CustomUser
 
+
 class Command(BaseCommand):
-    help = 'Create superuser for phone-based auth'
+    help = 'Force create superuser'
 
     def handle(self, *args, **kwargs):
-        phone    = '9999999999'
+        phone    = '9167164491'
         name     = 'Admin'
         password = 'Admin@1234'
 
-        if CustomUser.objects.filter(is_superuser=True).exists():
-            self.stdout.write(self.style.WARNING('Superuser already exists. Skipping.'))
-            return
+        # Delete all existing superusers
+        CustomUser.objects.filter(is_superuser=True).delete()
+        self.stdout.write(self.style.WARNING('Deleted old superusers.'))
 
-        CustomUser.objects.create_superuser(phone=phone, name=name, password=password)
-        self.stdout.write(self.style.SUCCESS(f'Superuser created: {phone}'))
+        # Create fresh one
+        user = CustomUser.objects.create_superuser(
+            phone=phone,
+            name=name,
+            password=password
+        )
+
+        # Force all flags to be correct
+        user.is_active    = True
+        user.is_staff     = True
+        user.is_superuser = True
+        user.save()
+
+        self.stdout.write(self.style.SUCCESS(f'Superuser created: {user.phone}'))
+        self.stdout.write(self.style.SUCCESS(f'is_active={user.is_active}'))
+        self.stdout.write(self.style.SUCCESS(f'is_staff={user.is_staff}'))
+        self.stdout.write(self.style.SUCCESS(f'is_superuser={user.is_superuser}'))
